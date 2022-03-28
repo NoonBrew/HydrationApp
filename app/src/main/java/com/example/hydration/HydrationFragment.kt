c41c1500cc1ac20636781d7955072a0f5144c9fd
PearMyst <email>
2022-03-28 17:33:23 -0500

    Added Validation in the Entity and the DaysRepository.kt

--- app/src/main/java/com/example/hydration/HydrationFragment.kt
+++ app/src/main/java/com/example/hydration/HydrationFragment.kt
@@ -18,7 +18,7 @@ private const val ARG_DAY_OF_WEEK = "day_of_week"
  * create an instance of this fragment.
  */
 class HydrationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private lateinit var dayOfWeek: String
 
     private lateinit var waterRatingBar: RatingBar
